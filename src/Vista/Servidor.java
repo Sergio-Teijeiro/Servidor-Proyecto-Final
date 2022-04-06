80bd0e4b8023ccd1934bc42c84f040a82c2e982e
admin <admin@DESKTOP-982SJBH>
2022-04-06 11:56:44 +0200

    Creación de la estructura general de la pantalla del servidor

--- src/Vista/Servidor.java
+++ src/Vista/Servidor.java
@@ -1,6 +1,9 @@
 package Vista;
 
 import java.awt.EventQueue;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 
@@ -8,6 +11,11 @@ public class Servidor extends JFrame {
 	
 	ServerSocket skServidor;
     ArrayList<Socket> listaSockets = new ArrayList<>();
+    private javax.swing.JButton btnDetener;
+    private javax.swing.JButton btnIniciar;
+    private javax.swing.JLabel lblEstadoPuerto;
+    private javax.swing.JLabel lblPuerto;
+    private javax.swing.JTextField txtPuerto;
 
 	/**
 	 * Launch the application.
@@ -31,7 +39,85 @@ public class Servidor extends JFrame {
 	public Servidor() {
 		setBounds(100, 100, 450, 300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+        lblEstadoPuerto = new javax.swing.JLabel();
+        lblPuerto = new javax.swing.JLabel();
+        txtPuerto = new javax.swing.JTextField();
+        btnIniciar = new javax.swing.JButton();
+        btnDetener = new javax.swing.JButton();
 
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("SERVIDOR");
+
+        lblEstadoPuerto.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        lblEstadoPuerto.setForeground(new java.awt.Color(255, 0, 0));
+        lblEstadoPuerto.setText("APAGADO");
+
+        lblPuerto.setText("Puerto");
+
+        txtPuerto.setEditable(false);
+        txtPuerto.setText("2000");
+
+        btnIniciar.setText("Iniciar");
+        btnIniciar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnIniciarActionPerformed(evt);
+            }
+        });
+
+        btnDetener.setText("Detener");
+        btnDetener.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDetenerActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(171, 171, 171)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(lblPuerto)
+                            .addGap(40, 40, 40)
+                            .addComponent(txtPuerto, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(lblEstadoPuerto, javax.swing.GroupLayout.PREFERRED_SIZE, 222, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(btnIniciar)
+                        .addGap(94, 94, 94)
+                        .addComponent(btnDetener)
+                        .addGap(25, 25, 25)))
+                .addContainerGap(200, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblEstadoPuerto, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblPuerto)
+                    .addComponent(txtPuerto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(82, 82, 82)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnIniciar)
+                    .addComponent(btnDetener))
+                .addContainerGap(135, Short.MAX_VALUE))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+	}
+	
+	private void btnIniciarActionPerformed(java.awt.event.ActionEvent evt) {
+		
+	}
+	
+	private void btnDetenerActionPerformed(java.awt.event.ActionEvent evt) {
+		
 	}
 
 }
