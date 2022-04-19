1d6d376c7766efebb216506a4d7607851dd9daad
admin <admin@DESKTOP-982SJBH>
2022-04-19 19:40:19 +0200

    Ejecutar inserción

--- src/Controlador/gestionNumeros.java
+++ src/Controlador/gestionNumeros.java
@@ -2,33 +2,41 @@ package Controlador;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 import Modelo.Numero;
 
 public class gestionNumeros {
 
 	public static void insertarNumero(Numero numero) {
-		String insercion = "INSERT INTO numeros (titulo,fecha_adquisicion,tapa,estado,resenha,img,id_coleccion VALUES (?,?,?,?,?,?,?);)";
-		InputStream input = new ByteArrayInputStream(numero.getImg());
-		
+		//String insercion = "INSERT INTO numeros (titulo,fecha_adquisicion,tapa,estado,resenha,img,id_coleccion) VALUES (?,?,?,?,?,?,?);";
+		String insercion = "INSERT INTO numeros (titulo,fecha_adquisicion,tapa,estado,resenha,id_coleccion) VALUES (?,?,?,?,?,?);";
+		//InputStream input = new ByteArrayInputStream(numero.getImg());
+		Connection con = null;
 		
 		try {
-			PreparedStatement ps = Pool.getConexion().prepareStatement(insercion);
+			con = Pool.getConexion();
+			PreparedStatement ps = con.prepareStatement(insercion);
 			
 			ps.setString(1, numero.getTitulo());
 			ps.setDate(2, numero.getFechaAdquisicion());
 			ps.setString(3, numero.getTapa());
 			ps.setString(4, numero.getEstado());
 			ps.setString(5, numero.getResenha());
-			ps.setBinaryStream(5, input, (int)(numero.getImg().length));
+			//ps.setBinaryStream(6, input, (int)(numero.getImg().length));
 			ps.setInt(6, numero.getIdColeccion());
 			 
 			ps.executeUpdate();
 			
+			con.commit();
+			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			try {
+				con.rollback();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			e.printStackTrace();
 		}  finally {
 			Pool.Cerrar();
