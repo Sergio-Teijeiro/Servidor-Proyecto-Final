ba485ba9d6f85528f5e11d0e6988e4db68085e16
admin <admin@DESKTOP-982SJBH>
2022-05-25 12:26:26 +0200

    Test

--- src/Vista/Servidor.java
+++ src/Vista/Servidor.java
@@ -2,27 +2,28 @@ package Vista;
 
 import java.awt.Color;
 import java.awt.EventQueue;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-import Controlador.HiloServidor;
-import Controlador.Pool;
-import javax.swing.JButton;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.ImageIcon;
-import javax.imageio.ImageIO;
-import javax.swing.GroupLayout;
-import javax.swing.LayoutStyle.ComponentPlacement;
+import Controlador.*;
+import Modelo.*;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class Servidor extends JFrame {
 	
@@ -134,6 +135,153 @@ public class Servidor extends JFrame {
 
         pack();
         setLocationRelativeTo(null);
+        
+        //insertarComics();
+	}
+	
+	private void insertarColeccion() {
+		try {
+            InputStream is = new FileInputStream("C:\\Users\\admin\\Downloads\\response.json");
+            try {
+				String jsonTxt = IOUtils.toString(is, "UTF-8");
+				JSONObject jsonObj = new JSONObject(jsonTxt);
+				JSONObject obj = (JSONObject) jsonObj.get("data");
+				
+				JSONArray array = obj.getJSONArray("results");
+				
+				for (int i=0;i<array.length();i++) {
+					JSONObject col = array.getJSONObject(i);
+					
+					if (col.getString("title").equals("The Invincible Iron Man (2004 - 2007)")) {
+						System.out.println(col.getString("title"));
+						
+						JSONObject thumbnail = col.getJSONObject("thumbnail");
+						
+						System.out.println(thumbnail.getString("path")+"."+thumbnail.getString("extension"));
+						
+						URL url = new URL(thumbnail.getString("path")+"."+thumbnail.getString("extension"));
+						InputStream in = url.openStream();
+						
+						byte[] imgBytes = IOUtils.toByteArray(in);
+						
+						Coleccion cole = new Coleccion (0,col.getString("title"),imgBytes);
+						
+						gestionColecciones.insertarColeccion(cole);
+						
+						break;
+					}
+				}
+				
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+	}
+
+	private void insertarComics() {
+		try {
+            InputStream is = new FileInputStream("C:\\Users\\admin\\Downloads\\response.json");
+            try {
+				String jsonTxt = IOUtils.toString(is, "UTF-8");
+				JSONObject jsonObj = new JSONObject(jsonTxt);
+				JSONObject obj = (JSONObject) jsonObj.get("data");
+				
+				JSONArray array = obj.getJSONArray("results");
+				String[] tapas = {"Dura","Blanda"};
+				String[] estados = {"Últimas páginas dobladas","Ya vino con una página gastada","Uso ligero","Tres páginas con manchas",
+						"Tiene un par de páginas rotas por las esquinas","Tapas como del primer día y contenido bien",
+						"Tapas arrugadas","Solo tiene una página mal","Solo tiene mal la última página","Sin ningún defecto","Sin defectos",
+						"Segunda mano","Regular","Recién comprado","Pésimo","Pérdida de color en algunas páginas","Página 3 rota","Página 2 un poco rota","Probablemente con poco uso",
+						"Primeras páginas ligeramente gastadas","Perfecto estado","Perfecto","Perdió un poco los colores","Nuevo","No tiene todas las páginas",
+						"No tiene defectos","No muy bueno","No está mal","Muy usado","Muy bueno, materiales muy resistentes","Muy bueno, de los mejores","Muy bueno",
+						"Muy buena calidad","Muy bien cuidado","Muy bien conservado","Malo","Lomos ligeramente gastados","Lomos gastados","Lomos descoloridos",
+						"Ligero rasguño","Ligero desgaste en algunas páginas","Ligeramente gastado","Ligera mancha en portada","Genial","Gastado","Fatal",
+						"Excelente para el uso dado","Excelente","Está bastante bien","El mejor de la colección","Con manchas al principio","Como nuevo",
+						"Calidad excelente","Buenísimo","Bueno, plastificado","Bueno, con tapas muy resistentes","Bueno","Bordes gastados por un ligero uso","Bien cuidado",
+						"Bastante bueno, aunque se nota que tiene uso","Bastante bueno","Bastante bien cuidado","Bastante bien conservado a pesar del uso","Bastante aceptable","Aceptable"};
+				byte[] imgBytes = null;
+				String resenha = null, titulo;
+				ArrayList<String> listaTitulos = new ArrayList<String>();
+				int contador = 2;
+				Date fecha;
+				
+				for (int i=0;i<array.length();i++) {
+					JSONObject col = array.getJSONObject(i);
+					
+					if (!col.isNull("title")) {
+						if (col.getString("title").equals("")) {
+							titulo = "";
+						} else {
+							titulo = col.getString("title");
+						}
+					} else {
+						titulo = "";
+					}
+					
+					if (listaTitulos.contains(titulo)) {
+						titulo += " " + contador;
+						listaTitulos.add(titulo);
+						contador++;
+					} else {
+						listaTitulos.add(titulo);
+					}
+					
+					System.out.println((i+1)+" - "+titulo);
+					
+					if (col.getString("modified").startsWith("-000")) {
+						fecha = new java.sql.Date(Calendar.getInstance().getTime().getTime());
+					} else {
+						fecha = Date.valueOf(col.getString("modified").substring(0, 10));
+					}
+					
+					if (!col.isNull("description")) {
+						if (col.getString("description").equals("") ) {
+							resenha = null;
+						} else {
+							resenha = col.getString("description");
+						}
+						
+					} else {
+						resenha = null;
+					}
+					
+					String tapa = tapas[(int) (Math.random() * tapas.length)];
+					String estado = estados[(int) (Math.random() * estados.length)];
+					
+					JSONObject thumbnail = col.getJSONObject("thumbnail");
+					
+					if (!thumbnail.getString("path").endsWith("image_not_available")) {
+						URL url = new URL(thumbnail.getString("path")+"."+thumbnail.getString("extension"));
+						InputStream in = url.openStream();
+							
+						imgBytes = IOUtils.toByteArray(in);
+					} else {
+						imgBytes = null;
+					}
+						
+					Numero numero = new Numero (0,titulo,fecha,tapa,estado,resenha,imgBytes,172);
+
+					gestionNumeros.insertarNumero(numero);
+
+				}
+				
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}		
 	}
 	
 	private void btnIniciarActionPerformed(java.awt.event.ActionEvent evt) {
@@ -200,4 +348,6 @@ public class Servidor extends JFrame {
             }
         } 		
 	}
+	
 }
+
